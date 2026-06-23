afd79421554c4cf2b1974f02e6c6d73de34eee35
sibprod <sibprod@live.fr>
2026-06-23 15:23:20 +0200

    Update service_etape1_T3_bilan_payload.js

--- services/visualisation/service_etape1_T3_bilan_payload.js
+++ services/visualisation/service_etape1_T3_bilan_payload.js
@@ -1,4 +1,4 @@
-// services/visualisation/bilanFablePayloadService.js
+// services/visualisation/service_etape1_T3_bilan_payload.js
 // Payload service — Bilan Fable cognitif complet (chaîne P-A→P-D)
 // v2.0 — 20/06/2026
 //
